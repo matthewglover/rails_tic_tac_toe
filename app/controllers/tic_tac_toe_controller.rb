467f434e703327c2e77aafb12ff991d9bd7d94f8
Matt Glover <matthewglover@me.com>
2017-09-29 11:51:50 +0100

    Add options and game routes (spike)

--- app/controllers/tic_tac_toe_controller.rb
+++ app/controllers/tic_tac_toe_controller.rb
@@ -3,5 +3,40 @@ class TicTacToeController < ApplicationController
   end
 
   def game
+    @game = build_game(params)
+    @game_options = {
+      action: :game,
+      player_x_type: params[:player_x_type],
+      player_o_type: params[:player_o_type],
+      moves: @game.board.moves.join(",")
+    }
+  end
+
+  private
+
+  def build_game(params)
+    MattsTictactoeCore::Game.new(
+      player_x_type: params[:player_x_type],
+      player_o_type: params[:player_o_type],
+      board: build_board(params))
+  end
+
+  def build_board(params)
+    moves = get_moves(params)
+    move = get_move(params)
+    board = MattsTictactoeCore::Board.new(moves)
+    active_game?(params) ? board.move(move) : board
+  end
+
+  def active_game?(params)
+    params.has_key?("move")
+  end
+
+  def get_moves(params)
+    params.fetch("moves", "").split(",").map(&:to_i)
+  end
+
+  def get_move(params)
+    params["move"].to_i
   end
 end
